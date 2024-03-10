e5c3b3d602a9c7c25424a59c4c8732d48dc59f4f
WhippaSnappers <tread.honeys0k@icloud.com>
2024-03-10 15:05:01 +1000

    Put files back in place

---
+++ DromEvaluation/Aux/Typedefs.h
@@ -0,0 +1,13 @@
+//
+//  Typedefs.h
+//  DromEvaluation
+//
+//  Created by Paul Cherevach on 07.03.2024.
+//
+
+#ifndef Typedefs_h
+#define Typedefs_h
+
+typedef void (^DownloadingTaskCompletionHandlerBlock)(NSData * data, NSURLResponse * response, NSError * error);
+
+#endif /* Typedefs_h */
---
+++ DromEvaluation/Model/ImagesLinksSource.h
@@ -0,0 +1,17 @@
+//
+//  ImagesLinksSource.h
+//  DromEvaluation
+//
+//  Created by Paul Cherevach on 07.03.2024.
+//
+
+#ifndef ImagesLinksSource_h
+#define ImagesLinksSource_h
+
+@protocol ImagesLinksSource
+
+@property(strong, readonly) NSArray *images;
+
+@end
+
+#endif /* ImagesLinksSource_h */
